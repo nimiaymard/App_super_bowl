c6b12f5c3e1568eaea57ea8b3b8e14deec229a55
NIMI <nimichristbert@gmail.com>
2024-07-22 08:28:48 +0200

    Add index.php composer.lock

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,12 @@
 <?php
 require_once 'db_connection.php';
 
-$query = $pdo->query("SELECT team1, team2, time, status FROM matches WHERE date = CURDATE()");
+// Récupérer les matchs du jour
+$query = $pdo->prepare("SELECT team1, team2, time, status FROM matches WHERE DATE(time) = CURDATE()");
+$query->execute();
 $matches = $query->fetchAll(PDO::FETCH_ASSOC);
 
+// Envoyer les données en JSON
 header('Content-Type: application/json');
 echo json_encode(['matches' => $matches]);
-
+?>
