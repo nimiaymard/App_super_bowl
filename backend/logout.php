399b951fb1c3e34393b25fd5162a2e4f4b4beae4
NIMI <nimichristbert@gmail.com>
2024-07-24 23:18:57 +0200

    Modification of logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
-header('Content-Type: application/json');
-echo json_encode(['success' => true]);
+header("Location: ../public/index.html");
+exit;
 ?>
